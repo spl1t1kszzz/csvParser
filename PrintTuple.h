eabfc1a3d9f8b1601881714702e46440e4c2f184
spl1t1kszzz <nik17403@gmail.com>
2023-01-19 21:47:44 +0700

    print_tuple created

--- PrintTuple.h
+++ PrintTuple.h
@@ -1,10 +1,29 @@
 #ifndef CSVPARSER_PRINTTUPLE_H
 #define CSVPARSER_PRINTTUPLE_H
 
+#include <iostream>
+#include <tuple>
+
+template <std::size_t> struct int_{};
 
 
+template <class Tuple, typename Ch, typename Tr>
+std::ostream& print_tuple(std::basic_ostream<Ch, Tr>& out, const Tuple& t, int_<1>) {
+    return out << std::get<std::tuple_size<Tuple>::value - 1>(t);
+}
 
+template<class Tuple, typename Ch, typename Tr, std::size_t Pos>
+std::ostream& print_tuple(std::basic_ostream<Ch, Tr>& out, const Tuple& t, int_<Pos>) {
+    out << std::get<std::tuple_size<Tuple>::value - Pos>(t) << ", ";
+    return print_tuple(out, t, int_<Pos-1>());
+}
 
+template<class... Args, typename Ch, typename Tr>
+auto operator<<(std::basic_ostream<Ch, Tr>& out, const std::tuple<Args...>& t) {
+   out << "(";
+   print_tuple(out, t, int_<sizeof...(Args)>());
+   return out << ")";
+}
 
 
 #endif //CSVPARSER_PRINTTUPLE_H
--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <string>
+#include <tuple>
+#include "PrintTuple.h"
 
 // Variadic templates
 // std::tuple
-
 // parameter pack - упаковка параметров
 
 
 using namespace std;
 
+
 template <class... Args>
-void print(Args... args) {
-    cout << "true" << endl;
+void print(Args... args) { }
+
+
+template <class Head, class... Args>
+void print(Head head, Args... args) {
+    cout << head << endl;
+    print(args...);
 }
 
-
 int main() {
-    print();
-    print(1);
-    print(1,2, "C++");
+    auto super_tuple = std::tuple_cat(std::make_tuple(1,2,3), std::make_tuple(4,5,6));
+    std::cout << super_tuple << std::endl;
     return 0;
 }
